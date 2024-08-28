40c84d339f500c2f9cc4ac226a690cae3f5b259d
NotTheCrocHunter <notthecrochunter@gmail.com>
2024-08-27 22:05:07 -0400

    Further tweaks to settings to serve react app index properly

--- weez_draftboard_web/settings.py
+++ weez_draftboard_web/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'), 
+            # os.path.join(BASE_DIR, 'templates'), 
             os.path.join(BASE_DIR, 'frontend', 'build')
             ],  # React build directory],
         'APP_DIRS': True,
