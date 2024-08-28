56c234b071f7f0c14a55b33a0ec796bbbce05ed3
NotTheCrocHunter <notthecrochunter@gmail.com>
2024-08-27 20:08:07 -0400

    Fix STATICFILES_DIRS path and resolve local errors.

--- weez_draftboard_web/settings.py
+++ weez_draftboard_web/settings.py
@@ -130,10 +130,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-     os.path.join(BASE_DIR, 'weez_draftboard_web/frontend/build/static'),  # React build files
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
